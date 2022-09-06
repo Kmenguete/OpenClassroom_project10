4fcfffcedb4aadd43e3a24577e1ccaf16e92eb86
Kmenguete <menguetekevin@gmail.com>
2022-09-06 11:16:27 +0200

    creating custom user model

--- issue_tracking_system/authentication/views.py
+++ issue_tracking_system/authentication/views.py
@@ -1,11 +1,11 @@
-from django.contrib.auth.models import User
-from rest_framework import generics
+from .models import User
+from rest_framework.views import APIView
 
 from .serializers import SignUpSerializer
 from rest_framework.permissions import AllowAny
 
 
-class SignUpView(generics.CreateAPIView):
+class SignUpView(APIView):
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = SignUpSerializer
