5c948c7ae80b1c03f70bda42ac6f4ddcbd169bfd
Kmenguete <menguetekevin@gmail.com>
2022-09-07 11:35:19 +0200

    creating Project Issue Contributor and Comment model

--- issue_tracking_system/projects/models.py
+++ issue_tracking_system/projects/models.py
@@ -1,3 +1,35 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.TextField(max_length=2048, blank=True)
+    type = models.CharField(max_length=128)
+    author = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+
+class Contributor(models.Model):
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    project = models.ForeignKey(to=Project, on_delete=models.CASCADE)
+    permission = models.BooleanField(default=False, blank=True)
+    role = models.CharField(max_length=128)
+
+
+class Issue(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.CharField(max_length=256)
+    tag = models.CharField(max_length=128)
+    priority = models.CharField(max_length=128)
+    project = models.ForeignKey(to=Project, on_delete=models.CASCADE)
+    status = models.CharField(max_length=128)
+    author = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='author')
+    assignee = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='assignee')
+    created_time = models.DateTimeField(auto_now_add=True)
+
+
+class Comments(models.Model):
+    description = models.CharField(max_length=256)
+    author = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    issue = models.ForeignKey(to=Issue, on_delete=models.CASCADE)
+    created_time = models.DateTimeField(auto_now_add=True)
