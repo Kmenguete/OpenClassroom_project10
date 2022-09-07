5219f1a7bcb11afaf148b0fbb7efac96a0c155c3
Kmenguete <menguetekevin@gmail.com>
2022-09-07 15:11:17 +0200

    creatied serializers for every Models

--- issue_tracking_system/projects/serializers.py
+++ issue_tracking_system/projects/serializers.py
@@ -1,16 +1,30 @@
 from rest_framework.serializers import ModelSerializer
-from .models import Project, Contributor
+from .models import Project, Contributor, Issue, Comment
 
 
 class ProjectSerializer(ModelSerializer):
 
     class Meta:
         model = Project
-        fields = ['title', 'description', 'type', 'author']
+        fields = ['title', 'description', 'type']
 
 
 class ContributorSerializer(ModelSerializer):
 
     class Meta:
         model = Contributor
-        fields = ['user', 'project', 'permission', 'role']
+        fields = ['project', 'role']
+
+
+class IssueSerializer(ModelSerializer):
+
+    class Meta:
+        model = Issue
+        fields = ['title', 'description', 'tag', 'priority', 'project', 'status', 'assignee']
+
+
+class CommentSerializer(ModelSerializer):
+
+    class Meta:
+        model = Comment
+        fields = ['description']
