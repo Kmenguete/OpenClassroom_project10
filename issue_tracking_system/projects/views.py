3624d35b21eceaf9e809baea38276c09a97c7936
Kmenguete <menguetekevin@gmail.com>
2022-09-07 16:26:03 +0200

    if the user is authenticated, he/she will have the possibility to retrieve every projects she/he is involved in as well as Author or Contributor

--- issue_tracking_system/projects/views.py
+++ issue_tracking_system/projects/views.py
@@ -1,6 +1,7 @@
+from django.contrib.auth.decorators import login_required
 from rest_framework.viewsets import ModelViewSet
 
-from .models import Project
+from .models import Project, Contributor
 from .serializers import ProjectSerializer
 
 
@@ -8,5 +9,7 @@ class ProjectViewSet(ModelViewSet):
 
     serializer_class = ProjectSerializer
 
-    def get_queryset(self):
-        return Project.objects.all()
+    @login_required
+    def get_queryset(self, request):
+        return Project.objects.filter(author=request.user), \
+               Contributor.objects.filter(user=request.user).values('project')
