67c2384d2a7098af52979f06a9c6af1cec925831
Kmenguete <menguetekevin@gmail.com>
2022-09-07 15:50:13 +0200

    creating ProjectViewSet in order to let the user to retrieve the list of every project he/she is involved in

--- issue_tracking_system/projects/views.py
+++ issue_tracking_system/projects/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Project
+from .serializers import ProjectSerializer
+
+
+class ProjectViewSet(ModelViewSet):
+
+    serializer_class = ProjectSerializer
+
+    def get_queryset(self):
+        return Project.objects.all()
