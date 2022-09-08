4ea8bc46cdb9c7c27e73722de031570aa768357c
Kmenguete <menguetekevin@gmail.com>
2022-09-08 17:25:20 +0200

    Nesting serializers

--- issue_tracking_system/projects/serializers.py
+++ issue_tracking_system/projects/serializers.py
@@ -1,13 +1,33 @@
 from django.contrib.auth.decorators import login_required
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 from .models import Project, Contributor, Issue, Comment
 
 
 class ProjectSerializer(ModelSerializer):
 
+    issues = serializers.SerializerMethodField()
+    contributors = serializers.SerializerMethodField()
+
     class Meta:
         model = Project
-        fields = ['title', 'description', 'type']
+        fields = ['title', 'description', 'type', 'issues', 'contributors']
+
+    def get_issues(self, instance):
+
+        queryset = instance.issues.filter(project=Project)
+
+        serializer = IssueSerializer(queryset, many=True)
+
+        return serializer.data
+
+    def get_contributors(self, instance):
+
+        queryset = instance.contributors.filter(project=Project)
+
+        serializer = ContributorSerializer(queryset, many=True)
+
+        return serializer.data
 
 
 class CreateProjectSerializer(ModelSerializer):
@@ -100,9 +120,19 @@ class DeleteContributorSerializer(ModelSerializer):
 
 class IssueSerializer(ModelSerializer):
 
+    comments = serializers.SerializerMethodField()
+
     class Meta:
         model = Issue
-        fields = ['title', 'description', 'tag', 'priority', 'project', 'status', 'assignee']
+        fields = ['title', 'description', 'tag', 'priority', 'project', 'status', 'assignee', 'comments']
+
+    def get_comments(self, instance):
+
+        queryset = instance.comments.filter(issue=Issue)
+
+        serializer = CommentSerializer(queryset, many=True)
+
+        return serializer.data
 
 
 class CreateIssueSerializer(ModelSerializer):
