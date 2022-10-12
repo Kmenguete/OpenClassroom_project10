adca2964a16616b9c412eda7fab9d710d5a6da34
Kmenguete <menguetekevin@gmail.com>
2022-10-12 14:20:03 +0200

    successfully reformatted code using black

--- issue_tracking_system/issue_tracking_system/urls.py
+++ issue_tracking_system/issue_tracking_system/urls.py
@@ -22,23 +22,32 @@ import authentication.views
 import projects.views
 
 projects_router = routers.SimpleRouter(trailing_slash=False)
-projects_router.register(r'project/?', projects.views.ProjectViewSet, basename="project")
-users_router = routers.NestedSimpleRouter(projects_router, r'project/?', lookup="project")
-users_router.register(r'users/?', projects.views.ContributorViewSet, basename="users")
-issues_router = routers.NestedSimpleRouter(projects_router, r'project/?', lookup="project")
-issues_router.register(r'issues/?', projects.views.IssueViewSet, basename="issues")
-comments_router = routers.NestedSimpleRouter(issues_router, r'issues/?', lookup="issues")
-comments_router.register(r'comments/?', projects.views.CommentViewSet, basename="comments")
+projects_router.register(
+    r"project/?", projects.views.ProjectViewSet, basename="project"
+)
+users_router = routers.NestedSimpleRouter(
+    projects_router, r"project/?", lookup="project"
+)
+users_router.register(r"users/?", projects.views.ContributorViewSet, basename="users")
+issues_router = routers.NestedSimpleRouter(
+    projects_router, r"project/?", lookup="project"
+)
+issues_router.register(r"issues/?", projects.views.IssueViewSet, basename="issues")
+comments_router = routers.NestedSimpleRouter(
+    issues_router, r"issues/?", lookup="issues"
+)
+comments_router.register(
+    r"comments/?", projects.views.CommentViewSet, basename="comments"
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('signup/', authentication.views.SignUpView.as_view(), name='sign_up'),
-    path('', include(projects_router.urls)),
-    path('', include(users_router.urls)),
-    path('', include(issues_router.urls)),
-    path('', include(comments_router.urls)),
-
+    path("admin/", admin.site.urls),
+    path("api-auth/", include("rest_framework.urls")),
+    path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("login/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("signup/", authentication.views.SignUpView.as_view(), name="sign_up"),
+    path("", include(projects_router.urls)),
+    path("", include(users_router.urls)),
+    path("", include(issues_router.urls)),
+    path("", include(comments_router.urls)),
 ]
